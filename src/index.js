59f26f84e711a5c0d00cb564215ac96bc4adf584
Jose Ulloa <jandres258@hotmail.com>
2020-01-26 14:37:35 -0600

    add frontend template

--- src/index.js
+++ src/index.js
@@ -3,3 +3,11 @@ dotenv.config()
 
 // db connection
 require('./database')
+
+// server
+const app = require('./server')
+
+// starting the server
+app.listen(app.get('port'), () => {
+    console.log(`server listening on port ${app.get('port')}`);
+})
---
+++ src/server.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const morgan = require('morgan')
+const path = require('path')
+const createError = require('http-errors')
+
+const app = express()
+
+// settings 
+app.set('port', process.env.PORT || 3000)
+
+// middlewares
+app.use(morgan('dev'))
+app.use(express.json())
+
+// static Files
+app.use(express.static(path.join(__dirname, '..', 'public')))
+
+
+// routes
+//app.use(process.env.API_PATH, require('./controllers/authentication.controller'))
+// this route is used to redirect to the web page
+app.use('*', (req, res, next) => {
+    if(!req.originalUrl.includes(process.env.API_PATH))
+        res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
+    else
+        next()
+})
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+    next(createError(404))
+})
+
+module.exports = app
