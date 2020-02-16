a0a1da400e776599b2dde2a798fea56faee66a06
Jose Ulloa <jandres258@hotmail.com>
2020-02-16 13:32:52 -0600

    Adding services to frontend

---
+++ pizzapp-frontend/src/services/ingredient.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BaseHttpService } from './BaseHttpService';
+import { Ingredient } from 'src/models/ingredient';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IngredientService extends BaseHttpService<Ingredient> {
+  constructor(private http: HttpClient) { 
+    super(http, '/ingredients')
+  }
+  
+}
