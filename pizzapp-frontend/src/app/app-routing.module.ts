31ed23ccda745c72d460caf6f1d639aa8f02beb2
Julian Diaz <jdiazh362@ulacit.ed.cr>
2020-02-23 15:16:09 -0600

    Add login form to test back-end

--- pizzapp-frontend/src/app/app-routing.module.ts
+++ pizzapp-frontend/src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'register', component: RegisterComponent }
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent }
+
 ];
 
 @NgModule({
--- pizzapp-frontend/src/app/app.module.ts
+++ pizzapp-frontend/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RegisterComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
