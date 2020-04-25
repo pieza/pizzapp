56b0e4819c33899a9ff1b538c4ce933c0ee6a7a8
Jose Ulloa <jandres258@hotmail.com>
2020-04-24 19:24:12 -0600

    admin fixes

--- src/models/order.js
+++ src/models/order.js
@@ -3,13 +3,11 @@ const { Schema } = mongoose
 
 const OrderSchema = new Schema({
     user_id: { type: Schema.Types.ObjectId, ref: 'User', require: true },
-    products: [{
-        product_id: { type: Schema.Types.ObjectId, ref: 'Product', require: true },
-        quantity: { type: Schema.Types.Number, default: 1 }
-    }],
+    products: [{ type: Schema.Types.ObjectId, ref: 'Product', require: true }],
     promo_id: { type: Schema.Types.ObjectId, ref: 'Promo' },
     status: { type: String },
-    date: { type: Date, default: Date.now }
+    date: { type: Date, default: Date.now },
+    total_price: { type: Number, default: 0 }
 })
 
 module.exports = mongoose.model('Order', OrderSchema)
