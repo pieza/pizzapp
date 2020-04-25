e1eb5a9cf1268429a69ea7db8d40dede539d4bc8
Julian Diaz <jdiazh362@ulacit.ed.cr>
2020-04-24 18:47:39 -0600

    Graph Fix

--- pizzapp-frontend/src/app/admin/pages/dashboard/dashboard.component.ts
+++ pizzapp-frontend/src/app/admin/pages/dashboard/dashboard.component.ts
@@ -3,6 +3,7 @@ import { Chart } from "chart.js";
 import { UserService } from "src/app/services/user.service";
 import { OrderService } from "src/app/services/order.service";
 import { PromoService } from "src/app/services/promo.service";
+import { promise } from 'protractor';
 
 @Component({
   selector: "app-dashboard",
@@ -25,26 +26,23 @@ export class DashboardComponent implements OnInit {
   repartidoresGraph = 0;
 
   ngOnInit(): void {
-    this.getStats();
+
     this.createChart();
   }
 
-  getStats() {
-    this.userService.find().subscribe((data) => {
-      this.userCounter = data.length;
-    });
-    this.orderService.find().subscribe((data) => {
-      this.orderCounter = data.length;
-    });
-    this.promoService.find({ active: true }).subscribe((data) => {
-      this.promoCounter = data.length;
-    });
-    this.userService.find({ type: "ADMIN" }).subscribe((data) => {
-      this.grandTotal = data.length;
-    });
+  async getStats() {
+    this.grandTotal = 0;
+    this.userCounter = (await this.userService.find().toPromise()).length;
+    this.orderCounter = (await this.orderService.find().toPromise()).length;
+    this.promoCounter = (await this.promoService.find({ active: true }).toPromise()).length;
+    this.sucursalGraph = (await this.userService.find({ type: "ADMIN" }).toPromise()).length;
+    this.repartidoresGraph = (await this.userService.find({ type: "EXPRESS" }).toPromise()).length;
+    this.clientsGraph = (await this.userService.find({ type: "CLIENT" }).toPromise()).length;
   }
 
-  createChart() {
+
+  async createChart() {
+    await this.getStats();
     // Set new default font family and font color to mimic Bootstrap's default styling
     (Chart.defaults.global.defaultFontFamily = "Nunito"),
       '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
@@ -56,7 +54,7 @@ export class DashboardComponent implements OnInit {
         labels: ["Clientes", "Sucursales", "Repartidores"],
         datasets: [
           {
-            data: [55, this.sucursalGraph, 15],
+            data: [this.clientsGraph, this.sucursalGraph, this.repartidoresGraph],
             backgroundColor: ["#4e73df", "#1cc88a", "#36b9cc"],
             hoverBackgroundColor: ["#2e59d9", "#17a673", "#2c9faf"],
             hoverBorderColor: "rgba(234, 236, 244, 1)",
